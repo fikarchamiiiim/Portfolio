ff2fba0c27d58f4267eda481a899a1df78166b69
Zulfikar Chamim <fikarchamim@yahoo.com>
2026-05-01 23:45:07 +0700

    change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: process.env.GITHUB_ACTIONS ? '/YOUR_REPO_NAME/' : '/',
+    base: process.env.GITHUB_ACTIONS ? '/Portfolio/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
